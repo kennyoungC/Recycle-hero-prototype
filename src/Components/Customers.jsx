7b0dd8708a7f86add9fa631c27bd78a10b37f25b
Kenneth <obikennethe913@gmail.com>
2022-06-09 15:28:53 +0300

    list of available customers ready for pickup

---
+++ src/Components/Customers.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import CustomerItems from "./CustomerItems"
+import MyNavbar from "./MyNavbar"
+
+const Customers = () => {
+  return (
+    <div>
+      <MyNavbar />
+      <CustomerItems />
+    </div>
+  )
+}
+
+export default Customers
